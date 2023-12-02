d2cd2a8f42c8cecc34ef291174dca69c00c0a7d6
Paris Pettis <pettis_paris@hotmail.com>
2023-12-02 18:14:20 +0200

    new todo and update todo for complete

--- app/src/app/new/page.tsx
+++ app/src/app/new/page.tsx
@@ -1,12 +1,25 @@
+import { prisma } from "@/db"
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function New() { 
+async function createTodo(data: FormData) {
+    "use server"
+    const title = data.get("title")?.valueOf()
+    if (typeof title !== "string" || title.length === 0) {
+        throw new Error("Invalid Title")
+    }
+
+    await prisma.todo.create({ data: {title, complete: false} })
+    redirect("/")
+}
+
+export default function Page() { 
     return (
         <>
             <header className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl">New</h1>
             </header>
-            <form className="flex gap-2 flex-col">
+            <form action={createTodo} className="flex gap-2 flex-col">
                 <input 
                     type="text" 
                     name="title" 
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,9 +6,14 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { complete } })
+} 
+
 export default async function Home() {
   const todos = await getTodos();
-  // await prisma.todo.create({ data: { title: "test", complete: false } })
   
   return ( 
     <> 
@@ -35,7 +40,7 @@ export default async function Home() {
 
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
